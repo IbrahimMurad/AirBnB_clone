39aa3ef6ef51879a6776eda546f8071f6b86db11
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-12-05 13:35:40 +0200

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -29,14 +29,27 @@ class BaseModel:
         of the instance.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize BaseModel attributes, id, created_at and updated_at
         """
 
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not len(kwargs) == 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    pass
+                elif key == 'created_at':
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.fromisoformat(value)
+                else:
+                    setattr(self, key, value)
+        if 'id' not in kwargs.keys():
+            self.id = uuid4()
+        if 'created_at' not in kwargs.keys():
+            self.created_at = datetime.now()
+        if 'updated_at' not in kwargs.keys():
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
